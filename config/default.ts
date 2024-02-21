286bd7e337d77e0df33538a949cf0de12a78a2e3
Fabricio Dias <fabricio270909@gmail.com>
2024-02-21 14:26:27 -0300

    Applying enviroment variables

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,7 @@
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
 export default {
     port: 3000,
-    dbUri: "mongodb+srv://fabricio_dias_log:kpSGBLuxRswN8rNz@cluster0.xo4lymf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.xo4lymf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 }
