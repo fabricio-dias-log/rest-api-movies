fe72c1b742350363ad5c942077bce529846ecdac
Fabricio Dias <fabricio270909@gmail.com>
2024-02-21 17:00:15 -0300

    Creating Model

---
+++ src/models/Movie.ts
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose";
+
+const MovieSchema = new Schema(
+    {   
+        title:{type: String, required: true},
+        rating: {type: Number, required: true},
+        description: {type: String, required: true},
+        director: {type: String, required: true},
+        stars: {type: Array, required: true},
+        poster: {type: String, required: true},
+    },
+    { 
+        timestamps: true 
+    }
+)
+
+export default model("Movie", MovieSchema);
