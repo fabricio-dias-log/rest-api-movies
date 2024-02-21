68103a092be79a05a417bb1738cb0257d1a144dd
Fabricio Dias <fabricio270909@gmail.com>
2024-02-21 17:22:39 -0300

    Insert data function

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -4,23 +4,12 @@ import Logger from "../../config/logger";
 
 export const createMovie = async (req: Request, res: Response) => {
     try {
-        const movie = new MovieModel(req.body);
-        console.log(movie);
-        // await movie.save();
-        // res.status(201).json(movie);
+        const data = req.body;
+        const movie = await MovieModel.create(data);
+        
+        res.status(201).json(movie);
     } catch (e: any) {
         Logger.error(e.message);
         res.status(500).send("Server Error");
     }
-
 }
-
-export const getMovies = async (req: Request, res: Response) => {
-    try {
-        const movies = await MovieModel.find();
-        res.status(200).json(movies);
-    } catch (e: any) {
-        Logger.error(e.message);
-        res.status(500).send("Server Error");
-    }
-};
