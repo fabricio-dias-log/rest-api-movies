4be7536e8eca35b9135baff8a1bf8f5c559c9364
Fabricio Dias <fabricio270909@gmail.com>
2024-02-21 14:03:40 -0300

    Intializing routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import config from 'config';
+import router from './router';
 
 const app = express();
 const port = config.get<number>('port');
 
 // JSON
 app.use(express.json());
+
+app.use(router);
 
 app.listen(port, async() => {
   console.log(`Server is running on port ${port}`);
