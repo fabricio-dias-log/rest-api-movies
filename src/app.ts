d9afc8dd3408170975947bd228f66ea62830c3fa
Fabricio Dias <fabricio270909@gmail.com>
2024-02-21 14:56:48 -0300

    Applying morgan to log requests

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,17 @@ import router from './router';
 import db from '../config/db';
 import Logger from '../config/logger';
 
+// middleware
+import morganMiddleware from './middleware/morgan.middleware';
+
 const app = express();
 const port = config.get<number>('port');
 
 // JSON
 app.use(express.json());
+app.use(morganMiddleware);
+app.use("/api",router);
 
-app.use(router);
 
 app.listen(port, async() => {
     await db();
