319c66e8d6b47d98f9c4cc17a941e7a362ad758f
Fabricio Dias <fabricio270909@gmail.com>
2024-02-21 13:31:17 -0300

    Express initial structure and config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import express from 'express';
+import config from 'config';
+
+const app = express();
+const port = config.get<number>('port');
+
+// JSON
+app.use(express.json());
+
+app.listen(port, async() => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
